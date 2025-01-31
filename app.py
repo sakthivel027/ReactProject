788d4b7c2995ae63d6a30eb455a8e5dca1cef170
Sakthivel <73012840+sakthivel027@users.noreply.github.com>
2025-01-31 19:52:57 +0530

    Create app.py
    added app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+#app.py
+#print("Hello, World!")
+class sample:
+  def __init__(self,num1,num2):
+    self.num1 = num1
+    self.num2 = num2
+  #@func  
+  def func(num1, num2):
+     #@static = numm
+    sum = num1 + num2
+    print(sum)
+    #@class
+    #obj.
+  func(12,12)
+
+a = 5 
+b = 'abc'
+print(type(a))
+print(type(b))
+
+
+
+
+
